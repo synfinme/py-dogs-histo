97b5a23972b454f040ed05ca52a17718dfdd24a4
Taras Korenko <taras.korenko@gmail.com>
2022-02-02 10:09:08 +0200

    Imports py-dogs-histo project

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+## Links
+1. Client setup, initialization: https://docs.datadoghq.com/developers/dogstatsd/?tab=kubernetes
+2. Usage: https://docs.datadoghq.com/metrics/dogstatsd_metrics_submission/
---
+++ docker/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+#from prometheus_client import start_http_server, Histogram
+#import os
+#import random
+#import sys
+#import time
+
+from datadog import initialize, statsd
+
+try:
+    from dataset import data
+except ImportError:
+    from default_dataset import data
+
+
+METRIC_NAME_BASE = 'py_dogstatsd_histo'
+METRIC_NAME_SUFFIX = os.environ.get('METRIC_NAME_SUFFIX', '')
+
+metric_name = METRIC_NAME_BASE
+if len(METRIC_NAME_SUFFIX) > 0:
+    metric_name += '_' + METRIC_NAME_SUFFIX
+
+options = {
+    'statsd_host': '127.0.0.1',
+    'statsd_port': 8125
+}
+
+initialize(**options)
+
+#h = Histogram(metric_name, 'Description of %s' % (metric_name))
+#
+#
+#def update_stats():
+#    # Populates 'h' metric with a bunch of values from imported dataset
+#    for v in data:
+#        h.observe(v)
+#
+#
+#def main():
+#    start_http_server(9100)
+#    x = 0
+#    while True:
+#        update_stats()
+#        x += 1
+#
+#        # Some cosmetic stuff
+#        sys.stdout.write('.')
+#        sys.stdout.flush()
+#        if (x % 60) == 59:
+#            sys.stdout.write('\n')
+#            sys.stdout.flush()
+#
+#        time.sleep(60)
+#
+#
+#if __name__ == '__main__':
+#    main()
